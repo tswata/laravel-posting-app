267d2bfba92003e84758676220f36554c26623c3
tswata <tswata@gmail.com>
2023-05-03 10:04:34 +0900

    よくわからないけどコミット

---
+++ database/migrations/2023_02_10_060158_create_g_o_a_l_s_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('g_o_a_l_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignId('user_ID')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('g_o_a_l_s');
+    }
+};
